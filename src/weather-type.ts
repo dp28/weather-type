88d32d79c531137a0678a101de7a7e68c79f8fa9
Daniel Patterson <dan24patt@gmail.com>
2016-05-19 08:08:23 +0100

    Build weather code from storm, preciptitation & cloud codes
    The codes are used in a specific order based on the idea that certain 
    components of weather are more important than others - i.e. that storms 
    are more important than any precipitation, which is in turn more important 
    than how cloudy it is.

--- src/weather-type.ts
+++ src/weather-type.ts
@@ -1,4 +1,18 @@
+import Storm from './storm/storm';
+import Clouds from './clouds/clouds';
+import Precipitation from './precipitation/precipitation';
+
 export default class WeatherType {
-  constructor(public raw: string) {
+
+  constructor(
+    public stormLevel: Storm,
+    public precipitation: Precipitation,
+    public cloudLevel: Clouds
+  ) {}
+
+  public toCode(): string {
+    return this.stormLevel.toCode().unwrapOrElse(() => (
+      this.precipitation.toCode().unwrapOrElse(() => this.cloudLevel.toCode()))
+    );
   }
 }
--- test/weather-type-spec.ts
+++ test/weather-type-spec.ts
@@ -2,18 +2,61 @@
 
 import { expect } from 'chai';
 import WeatherType from '../src/weather-type';
+import Storm from '../src/storm/storm';
+import Clouds from '../src/clouds/clouds';
+import Precipitation from '../src/precipitation/precipitation';
+import { Some, None } from 'option-t';
 
 describe('WeatherType', () => {
-  let subject: WeatherType;
-  let raw: string = 'test';
+  let stormLevel = new Storm(0);
+  let precipitation = new Precipitation(0, 0, 0);
+  let cloudLevel = new Clouds(0);
 
-  beforeEach(function () {
-    subject = new WeatherType(raw);
-  });
+  function buildCode(): string {
+    return new WeatherType(stormLevel, precipitation, cloudLevel).toCode();
+  }
 
-  describe('#raw', () => {
-    it('should add two numbers together', () => {
-      expect(subject.raw).to.equal(raw);
+  function stubReturning<T>(value: T): () => T {
+    return () => value;
+  }
+
+  describe('#toCode', () => {
+
+    context('when the Storm can be translated into a code', () => {
+      let stormCode = 'storm_code';
+
+      before(() => stormLevel.toCode = stubReturning(new Some(stormCode)));
+
+      it('should return that code', () => {
+        expect(buildCode()).to.equal(stormCode);
+      });
+    });
+
+    context('when the Storm cannot be translated into a code', () => {
+      before(() => stormLevel.toCode = stubReturning(new None<string>()));
+
+      context('when the Precipitation can be translated into a code', () => {
+        let rainCode = 'rain_code';
+
+        before(() => precipitation.toCode = stubReturning(new Some(rainCode)));
+
+        it('should return that code', () => {
+          expect(buildCode()).to.equal(rainCode);
+        });
+      });
+
+      context('when the Precipitation cannot be translated into a code', () => {
+        let cloudCode = 'cloud_code';
+
+        before(() => {
+          precipitation.toCode = stubReturning(new None<string>())
+          cloudLevel.toCode = stubReturning(cloudCode);
+        });
+
+        it('should return the Clouds code', () => {
+          expect(buildCode()).to.equal(cloudCode);
+        });
+      });
     });
   });
 });
