4ba638a3fdd70bb6a0f52eda54256f83abf7edc8
Daniel Patterson <dan24patt@gmail.com>
2016-05-21 16:50:10 +0100

    Export components from main entry point

--- src/weather-type.ts
+++ src/weather-type.ts
@@ -1,11 +1,29 @@
 import { Option, Some, None } from 'option-t';
 
 import Storm from './storm/storm';
+import { Level as StormLevel } from './storm/storm';
 import { extractStorm } from './storm/storm-parser';
 import Clouds from './clouds/clouds';
+import { Level as CloudLevel } from './clouds/clouds';
 import { extractClouds } from './clouds/clouds-parser';
 import Precipitation from './precipitation/precipitation';
 import { extractPrecipitation } from './precipitation/precipitation-parser';
+import {
+  Level as PrecipitationLevel,
+  Duration as PrecipitationDuration,
+  Type as PrecipitationType
+} from './precipitation/precipitation';
+
+export {
+  Clouds,
+  CloudLevel,
+  Precipitation,
+  PrecipitationLevel,
+  PrecipitationDuration,
+  PrecipitationType,
+  Storm,
+  StormLevel
+}
 
 export default class WeatherType {
 
