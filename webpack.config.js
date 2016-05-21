d4b7b27cbbd4cedc70f425050f5b68aac411e499
Daniel Patterson <dan24patt@gmail.com>
2016-05-21 18:25:07 +0100

    Export as a UMD library

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   entry: './src/weather-type.ts',
   output: {
-    filename: './dist/weather-type.js'
+    filename: './dist/weather-type.js',
+    library: 'weather-type',
+    libraryTarget: 'umd'
   },
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
